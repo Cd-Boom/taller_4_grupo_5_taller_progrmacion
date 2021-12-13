925eb17e4327c6d702288f1ec53e8a75ee8de1ec
Cd-Boom <80863675+Cd-Boom@users.noreply.github.com>
2021-12-13 18:02:44 -0500

    Update AsynAwaitEjemplo1.js

--- AsynAwaitEjemplo1.js
+++ AsynAwaitEjemplo1.js
@@ -2,16 +2,37 @@ const fs = require('fs');
 const _directory  = './';
 const _findKeyWord  = '.txt';
 
+/*SearchFiles ( function (directory,findKeyWord) {
+    let files = fs.readdirSync(directory).filter(fn => fn.endsWith(findKeyWord));
+    WriteInfiles (files, function(WroteFiles) {
+        let i = 1;
+        for(file in files) {
+            fs.writeFileSync(file,`Value : ${i}`);
+            i +=1;
+        }
+    });
+
+});*/
+
+
 let SearchInnerFilesByExtension  = async  ( directory,findKeyWord) => {
     let files = fs.readdirSync(directory).filter(fn => fn.endsWith(findKeyWord));
     return files;
 };
 
+let WriteInFiles = async (file,value) => {
+     fs.writeFileSync(file,`Value : ${value}`);
+     return file;
+};
+
 async  function main ()  {
-    const files = await SearchInnerFilesByExtension(_directory,_findKeyWord);
+    let files = await SearchInnerFilesByExtension(_directory,_findKeyWord);
     let filesInString = '';
+    let i = 1;
     for (file in files ) {
-        filesInString += `${file} \n`;
+        let path  = await WriteInFiles(`${_directory}carpeta/${file}${_findKeyWord}`,i);
+        filesInString += `${path} \n`;
+        i+=1;
     }
     console.info(filesInString);
 }
